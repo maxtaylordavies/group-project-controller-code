fdd2e3faab1a917da5397221fec13ca04682a125
Max Taylor-Davies <maxtaylordavies@gmail.com>
2024-02-09 16:34:14 +0000

    implements render() and close() on environment

--- src/environment.py
+++ src/environment.py
@@ -1,6 +1,7 @@
 import gym
 from gym import spaces
 import numpy as np
+import pygame
 
 from .constants import (
     MAX_VELOCITY,
@@ -15,9 +16,12 @@ from .constants import (
 class Environment(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
-    def __init__(self, render_mode=None, width=6, length=20):
+    def __init__(self, render_mode=None, width=6, length=24):
         self.width, self.length = width, length
-        self.window_size = 512
+        self.window_size = (
+            512,
+            int(512 * (width / length)),
+        )
 
         """
         define observation space as consisting of the car's current (continuous)
@@ -133,3 +137,54 @@ class Environment(gym.Env):
             self._render_frame()
 
         return observation, reward, terminated, False, info
+
+    def render(self):
+        if self.render_mode == "rgb_array":
+            return self._render_frame()
+
+    def _render_frame(self):
+        if self.window is None and self.render_mode == "human":
+            pygame.init()
+            pygame.display.init()
+            self.window = pygame.display.set_mode((self.window_size, self.window_size))
+        if self.clock is None and self.render_mode == "human":
+            self.clock = pygame.time.Clock()
+
+        canvas = pygame.Surface(self.window_size)
+        canvas.fill((255, 255, 255))
+        pix_size = self.window_size[0] / self.length
+
+        # draw track
+        pygame.draw.rect(
+            canvas,
+            (0, 0, 0),
+            pygame.Rect(
+                0,
+                self.window_size[1] / 2 - self.width / 2,
+                self.window_size[0],
+                self.width,
+            ),
+        )
+
+        # draw car
+        car_x = int(self._car_pos[0] * pix_size)
+        car_y = int(self.window_size[1] / 2 - self._car_pos[1] * pix_size)
+
+        if self.render_mode == "human":
+            # The following line copies our drawings from `canvas` to the visible window
+            self.window.blit(canvas, canvas.get_rect())
+            pygame.event.pump()
+            pygame.display.update()
+
+            # We need to ensure that human-rendering occurs at the predefined framerate.
+            # The following line will automatically add a delay to keep the framerate stable.
+            self.clock.tick(self.metadata["render_fps"])
+        else:  # rgb_array
+            return np.transpose(
+                np.array(pygame.surfarray.pixels3d(canvas)), axes=(1, 0, 2)
+            )
+
+    def close(self):
+        if self.window is not None:
+            pygame.display.quit()
+            pygame.quit()
