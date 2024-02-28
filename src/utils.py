ec09077822000edff1b6fc53baaf6bb2845c689a
Max Taylor-Davies <34690655+maxtaylordavies@users.noreply.github.com>
2024-02-28 16:13:08 +0000

    adds script to extract trained actor network and export to onnx (#5)

---
+++ src/utils.py
@@ -0,0 +1,8 @@
+import numpy as np
+import torch
+
+
+def to_numpy(tensor: torch.Tensor) -> np.ndarray:
+    return (
+        tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+    )
